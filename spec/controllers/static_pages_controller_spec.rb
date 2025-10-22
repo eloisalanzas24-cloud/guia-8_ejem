2e748fb223dbd282fd289bb15233fd2f7204434f
Eloisa Lanzas <eloisalanzas24@gmail.com>
2025-10-21 21:56:41 -0600

    Agrgamos el nuevo codigo en static_pages_controller

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
   
   describe "GET #home" do
     it "returns http success" do

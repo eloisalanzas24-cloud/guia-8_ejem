b4b85183c890d05ab9c56a69da9ffd355ac765b9
Eloisa Lanzas <eloisalanzas24@gmail.com>
2025-10-28 21:25:40 -0600

    Creamos las Rutas con Nombre

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "static_pages#home"
+  root 'static_pages#home'
+get '/help', to: 'static_pages#help'
+get '/about', to: 'static_pages#about'
+get '/contact', to: 'static_pages#contact'
 end

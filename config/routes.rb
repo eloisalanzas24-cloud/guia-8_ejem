14109010aaa52498a5e796758f69ba8ccdf17a5f
Eloisa Lanzas <eloisalanzas24@gmail.com>
2025-10-28 21:49:44 -0600

    Agregamos las rutas con nombre para navegar

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'static_pages#home'
-get '/help', to: 'static_pages#help'
-get '/about', to: 'static_pages#about'
-get '/contact', to: 'static_pages#contact'
+  get 'help', to: 'static_pages#help'
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
 end
